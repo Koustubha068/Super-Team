ec0b1600f0b39bfd97ebbf0a54276c40d2c7d4ad
Koustubha068 <KoustuK@gmail.com>
2024-05-07 09:31:41 -0400

    added styling with background

--- script.js
+++ script.js
@@ -1,44 +1,43 @@
-const teamMembers = [{
-    name: 'Lionel Messi',
-    position: 'Forward',
-    skills: ['Dribbling', 'Shoothing', 'Passing'],
-    strengths: 'Ball control',
-    weaknesses: 'Heading Ability',
-    biography: 'Lionel Messi is considereed on the the greatest football players',
-
-
-},
-{
-    name: 'Lionel Messi Jr',
-    position: 'Forward',
-    skills: ['Dribbling', 'Shoothing', 'Passing'],
-    strengths: 'Ball control',
-    weaknesses: 'Heading Ability',
-    biography: 'Lionel Messi is considereed on the the greatest football players',
-}
-
-]
+const teamMembers = [
+  {
+    name: "Lionel Messi",
+    position: "Forward",
+    skills: ["Dribbling", "Shoothing", "Passing"],
+    strengths: "Ball control",
+    weaknesses: "Heading Ability",
+    biography:
+      "Lionel Messi is considereed on the the greatest football players",
+  },
+  {
+    name: "Lionel Messi Jr",
+    position: "Forward",
+    skills: ["Dribbling", "Shoothing", "Passing"],
+    strengths: "Ball control",
+    weaknesses: "Heading Ability",
+    biography:
+      "Lionel Messi is considereed on the the greatest football players",
+  },
+];
 function generateTeamCards() {
-    const teamCardsContainer = document.getElementById('teamCards')
-    teamMembers.forEach(member => {
-        const card = document.createElement('div')
-        card.classList.add('col-md-4')
+  const teamCardsContainer = document.getElementById("teamCards");
+  teamMembers.forEach((member) => {
+    const card = document.createElement("div");
+    card.classList.add("col-md-4");
 
-//styling card based on position:
-let backgroundColor
+    //styling card based on position:
+    let backgroundColor;
+    switch (member.position) {
+      case "forward":
+        backgroundColor = "#EF767A";
+    }
+    card.style.backgroundColor = backgroundColor;
 
+    //create a list of skils with li tags
+    const skillsList = member.skills
+      .map((skill) => `<li> ${skill} </li>`)
+      .join("");
 
-//create a list of skils with li tags
-const skillsList =member.skills.map(skill => `<li> ${skill} </li>`).join('')
-
-
-
-
-
-
-
-
-        card.innerHTML = `
+    card.innerHTML = `
     <div class= "card">
     <div class ="card-header"> ${member.name}</div>
     <div class="card-body">
@@ -47,18 +46,17 @@ const skillsList =member.skills.map(skill => `<li> ${skill} </li>`).join('')
     <ul>
     ${skillsList}
     </ul>
-    <p><strong><Strengths:</strong> ${member.strengths}</p>
-    <p><strong><Weaknesses:</strong> ${member.weaknesses}</p>
-    <p><strong><Biography:</strong> ${member.biography}</p>
+    <p><strong>Strengths:</strong> ${member.strengths}</p>
+    <p><strong>Weakness:</strong> ${member.weaknesses}</p>
+    <p><strong>Biography:</strong> ${member.biography}</p>
 
 
     
 
     </div>
     </div>
-    `
-        teamCardsContainer.appendChild(card)
-    })
+    `;
+    teamCardsContainer.appendChild(card);
+  });
 }
-window.onload = generateTeamCards()
-
+window.onload = generateTeamCards();
