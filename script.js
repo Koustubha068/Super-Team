bb4e2a2ee24fbd60b028ef7d8d0cede1e222cd43
Koustubha068 <KoustuK@gmail.com>
2024-06-02 20:19:50 -0400

    finished of 1/3 of characters descriptions using objects

--- script.js
+++ script.js
@@ -5,28 +5,33 @@ const teamMembers = [
     strengths: 'Communication, Intelligence, Honest',
     weaknesses: 'Germaphobic, Socializing',
     description : 'Sheldon Cooper, a child prodigy with a towering intellect and an insatiable curiosity, navigates the complexities of growing up in East Texas while pursuing his passion for science and learning.',
-    img: ''
+    img: 'images/sheldon.png'
   },
   {
-    name: 'Hrithik Roshan',
-    position: 'All-rounder',
-    skills: ['Acting', 'Dancing', 'Modeling'],
-    strengths: 'Physique, Dance skills, Screen presence',
-    weaknesses: 'Typecasting, Public perception',
-    biography: 'Hrithik Roshan is an Indian actor known for his dancing skills and performance in Hindi films.',
-    bestMovie: 'Koi Mil Gaya',
-    img: 'image/hritick.PNG'
+    name: 'Pastor Jeff',
+    skills: ['Good at Public Speaking', 'He is a very good mentor', 'Very good at handling challenges'],
+    strengths: 'Communication, Emphatethic, Great Leadership',
+    weaknesses: 'naive, avoids conflict,overcommittment',
+    description : 'Pastor Jeff Difford is a well-meaning and empathetic minister in "Young Sheldon," known for his engaging sermons, strong moral compass, and ability to connect with his congregation through humor and compassion, despite occasional naivety and a tendency to avoid confrontation',
+    img: 'images/pastor jef.png'
   },
   {
-    name: 'Siddharth Malhotra &#11088 ( Captain) ',
-    position: 'Batsman',
-    skills: ['Acting', 'Modeling'],
-    strengths: 'Looks, Versatility',
-    weaknesses: 'Limited filmography',
-    biography: 'Siddharth Malhotra is an Indian actor and former model who appears in Hindi films.',
-    bestMovie: 'Student of the Year',
-    img: 'image/sid.PNG'
+    name: 'Paige',
+    skills: ['Advanced Intellectual Abilities', 'Analytical and Problem-Solving Skills', 'Creativity and Innovation'],
+    strengths: 'Communication, Intelligence, Curiosity',
+    weaknesses: 'Difficulty of Fitting In, Overthinking',
+    description : 'Paige Swanson is a highly intelligent and talented child prodigy who excels in academics, particularly in mathematics and science, and shares a friendly rivalry with Sheldon Cooper. Despite her impressive intellect, she faces significant emotional challenges, including pressure to perform, social isolation, and family tensions, which add depth to her character in "Young Sheldon."',
+    img: 'images/paige.png'
   },
+  {
+    name: 'Missy Cooper',
+    skills: ['Social Intelligence', 'Creativity', 'Sense of Humour'],
+    strengths: 'Communication, Physically Strong, Curiosity',
+    weaknesses: 'Impatient, Overconfidence',
+    description : 'Missy Cooper, Sheldon's twin sister in "Young Sheldon," is a socially adept and athletically talented teenager who balances her competitive spirit with a caring nature, often providing support and levity within the Cooper family dynamic. Despite her strengths, she grapples with impulsiveness and a search for direction, adding depth to her character's journey of self-discovery.',
+    img: 'images/missy cooper.png'
+  },
+  
 function generateTeamCards() {
   const teamCardsContainer = document.getElementById("teamCards");
   teamMembers.forEach((member) => {
